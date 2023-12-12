112d207100474bef35ac19eb6d0648f183287ee4
Priyapron <151008882+Priyapron@users.noreply.github.com>
2023-12-12 22:03:01 +0700

    Update main_test.go

--- exam-1/main_test.go
+++ exam-1/main_test.go
@@ -2,7 +2,7 @@ package main
 
 import "testing"
 
-func TestHelloFunction(t *testing.T) {
+func TestExampleFunction(t *testing.T) {
 	// TODO: Implement tests for the student's code
 	// Example test (adjust according to the actual assignment)
 	expected := "Hello"
@@ -29,29 +29,5 @@ func TestUserLogin(t *testing.T) {
 	expected := true
 	if got := UserLogin("admin", "password"); got != expected {
 		t.Errorf("UserLogin(\"admin\", \"admin\") = %v, want %v", got, expected)
-	}
-
-	// ทดสอบ username และ password ที่ถูกต้อง
-	result := UserLogin("user123", "pass456")
-	if !result {
-		t.Errorf("Expected true, but got false")
-	}
-
-	// ทดสอบ username ถูกต้องแต่ password ไม่ถูกต้อง
-	result = UserLogin("user123", "wrongpass")
-	if result {
-		t.Errorf("Expected false, but got true")
-	}
-
-	// ทดสอบ username ไม่ถูกต้องและ password ถูกต้อง
-	result = UserLogin("wronguser", "pass456")
-	if result {
-		t.Errorf("Expected false, but got true")
-	}
-
-	// ทดสอบ username และ password ทั้งคู่ไม่ถูกต้อง
-	result = UserLogin("wronguser", "wrongpass")
-	if result {
-		t.Errorf("Expected false, but got true")
 	}
 }
